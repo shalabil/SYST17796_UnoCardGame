a80818915865a48fcd70b489f78c212bb8e27dfd
lina-test <shalabil@sheridancollege.ca>
2020-04-21 20:14:26 -0400

    updated code with test methods

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -2,63 +2,90 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
- * 
  */
 package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  * @author dancye, 2018
- * audit lina shalabi, 2020.
- * most updated version2
- * :)
  */
-public abstract class Player 
+public class Player 
 {
-    
-    private String userName; //the unique ID for this player
+    private String playerID; //the unique ID for this player
+    List<Card> hand;
     
     /**
-     * A constructor that allows you to set the player's unique name
-     * @param name the unique name to assign to this player.
+     * A constructor that allows you to set the player's unique ID
+     * @param name the unique ID to assign to this player.
      */
+    
+    
     public Player(String name)
     {
-        userName= name;
+        playerID= name;
+    }
+
+
+
+    public Player() {
+        this.hand = new ArrayList<Card>();
+    }
+
+    public List<Card> getHandOfCards() {
+        return hand;
+    }
+
+    /**
+     * @return the playerID
+     */
+    public String getPlayerID() 
+    {
+        return playerID;
     }
     /**
-     * a method to test the length of the user name. it should be more than 3 
-     * characters
+     * A method to check that the palyerId length is 3 letters or more
      * @param name
      * @return 
      */
-    public static boolean checkName(String name){
+     public static boolean checkId(String name){
         if (name.length() >= 3) {
             return true;
         }
         return false;
     }
-    
-    public String getuserName() 
-    {
-        return userName;
+      /**
+     * A method that check if the password does not have any special character
+     * @param pass
+     * @return 
+     */
+
+    public static boolean checkChar(String name) {
+        for (int i = 0; i < name.length(); i++) {
+            int ch = name.charAt(i);
+            if (!Character.isLetterOrDigit(ch))
+            return true;
+        }
+        return false;
     }
+     
 
     /**
-     * Ensure that the userName is unique
-     * @param givenName the userName to set
+     * Ensure that the playerID is unique
+     * @param givenID the playerID to set
      */
-    public void setuserName(String givenName) 
+    public void setPlayerID(String givenID) 
     {
-      
-        userName = givenName;
+        playerID = givenID;
     }
     
-    
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
+  @Override
+    public String toString() {
+        return "Player{" +
+                "hand=" + hand +
+                '}';
+    }
     
 }
